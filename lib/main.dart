5f8556bb042ff62182c7d66ba3128a21f3aa77fb
Luan Gustavo Fortunato <Luan.Fortunato@proway.treina>
2022-07-05 11:06:13 -0300

    [update] finished first home page container

--- lib/main.dart
+++ lib/main.dart
@@ -16,45 +16,130 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('App Treino'),
-      ),
       body: Column(
         children: [
+          //Top
           Container(
-            height: 200,
-            width: 300,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 40,
+            ),
+            margin: const EdgeInsets.all(20),
             decoration: BoxDecoration(
-              color: Colors.orange,
+              color: Colors.white,
               borderRadius: BorderRadius.circular(30),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.blueGrey.shade100,
+                  blurRadius: 6,
+                  spreadRadius: 4,
+                  offset: const Offset(0, 5),
+                ),
+              ],
             ),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text('1 Feb 2019'),
-                Row(
-                  children: const [
-                    Icon(Icons.sunny),
-                    Text('Cloudy'),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.only(left: 56, bottom: 7),
+                  child: Text(
+                    '1 Feb 2019',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey.shade700,
+                    ),
+                  ),
                 ),
                 Row(
                   children: [
+                    const SizedBox(width: 20),
+                    Icon(
+                      Icons.cloud,
+                      color: Colors.amber.shade500,
+                      size: 25,
+                    ),
+                    const SizedBox(width: 10),
+                    const Text(
+                      'Cloudy',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: const [
-                        Text('26°'),
-                        Text('Indoor temp'),
+                        Text(
+                          '26°C',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Indoor Temp',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
                       ],
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: const [
-                        Text('26°'),
-                        Text('Indoor temp'),
+                        Text(
+                          '48.2%',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Outdoor Humidity',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
                       ],
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: const [
-                        Text('26°'),
-                        Text('Indoor temp'),
+                        Text(
+                          '52.99%',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Indoor temp',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
                       ],
                     ),
                   ],
